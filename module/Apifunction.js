55f5056b0c2786c7f020a96531381bf8f748a219
muskan2532150 <rg2532150@gmail.com>
2022-09-01 23:35:29 +0530

    Changes typography. background

--- module/Apifunction.js
+++ module/Apifunction.js
@@ -21,14 +21,33 @@ class Api {
     await fetch(url)
       .then((response) => response.json())
       .then((data) => data.result.forEach((el) => {
-        const li = document.createElement('li');
-        li.setAttribute('class', 'ul-li');
-        const liText = document.createTextNode(`${el.user}:  ${el.score}`);
-        li.appendChild(liText);
-        ul.appendChild(li);
+       createLi(el,ul);
       }));
     Box.appendChild(ul);
   }
 }
+
+const createLi = (el,ul) => {
+  const li = document.createElement('li');
+  li.setAttribute('class', 'ul-li');
+  const i = document.createElement('i');
+  const div = document.createElement('div');
+  const div1 = document.createElement('div');
+  i.setAttribute('class', 'fa-solid fa-award');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p1Text = document.createTextNode(`${el.user}:`);
+  const p2Text = document.createTextNode(`${el.score}`);
+  p1.appendChild(p1Text);
+  p2.appendChild(p2Text);
+  
+  div.appendChild(i);
+  div1.appendChild(p1);
+  div1.appendChild(p2);
+  div.appendChild(div1);
+  li.appendChild(div);
+  ul.appendChild(li);
+}
+
 
 export default Api;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import './media.css';
 import {
   Form, Name, refresh, Score,
 } from '../module/Import.js';
